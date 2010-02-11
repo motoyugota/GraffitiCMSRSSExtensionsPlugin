c49bfa5b4e1ba902b9d8ecb3d857f13757851c08
charlesboyung_cp <SND\charlesboyung_cp>
2010-02-11 07:04:22 +0000

    Minor cleanup and trying to get folder name change to take.

--- Graffiti.RssExtensions/RssExtensionsPlugin.cs
+++ Graffiti.RssExtensions/RssExtensionsPlugin.cs
@@ -12,11 +12,8 @@
  * ***************************************************************************/
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Graffiti.Core;
 using System.Collections.Specialized;
+using Graffiti.Core;
 
 namespace Graffiti.RssExtensions
 {
